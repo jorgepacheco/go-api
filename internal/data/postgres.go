9dc247dbaedce264888d1d6ce90f8dc9d3273809
Jorge Pacheco Mengual <>
2020-09-01 16:46:51 +0200

    Add Heroku postgres database

--- internal/data/postgres.go
+++ internal/data/postgres.go
@@ -12,9 +12,9 @@ import (
 func getConnection() (*sql.DB, error) {
 
 	// postgres://gopher:gopher@127.0.0.1:5432/microblog?sslmode=disable"
-	uri := os.Getenv("DATABASE_URI")
+	uri := os.Getenv("DATABASE_URL")
 
-	fmt.Printf("DATABASE: " + uri)
+	fmt.Printf("URL FROM HEROKU DATABASE: " + uri)
 
 	if uri == "" {
 		uri = "postgres://admin:admin@127.0.0.1:5432/accounts?sslmode=disable"
